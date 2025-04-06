e9b4f02c00b1e29819f1da3ced9185a18b7c1847
SVVVG3 <SVVVG3.x@gmail.com>
2025-04-06 02:08:05 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/.well-known/farcaster.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+      {
         source: '/api/:path*',
         headers: [
           {
