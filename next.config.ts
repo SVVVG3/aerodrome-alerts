f8e202c4cd721e3ed127f9a7108a6745e0f81224
SVVVG3 <SVVVG3.x@gmail.com>
2025-04-06 02:11:38 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -24,23 +24,6 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type',
-          },
-        ],
-      },
     ]
   },
   images: {
